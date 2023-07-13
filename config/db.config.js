13b8556dfa9bd62fb0d9c3df3f5f18444236d464
chantelle mckenzie <mrspasceri@hotmail.com>
2023-07-13 11:33:26 -0700

    updated env file

--- config/db.config.js
+++ config/db.config.js
@@ -4,16 +4,12 @@ require("dotenv").config();
 let dbConn = null;
 function handleDisconnect() {
   dbConn = mysql.createConnection({
-    // host: process.env.DB_HOST,
-    // user: process.env.DB_USER,
-    // password: process.env.DB_PASSWORD,
-    // database: process.env.DB_DATABASE,
-    host: 'us-cdbr-east-06.cleardb.net',
-    user: 'bceb0bbaa4f9af',
-    password: 'acd65c6b',
-    database: 'heroku_ec7f6ec0f6cd80c',
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_DATABASE,
   });
-  mysql://bceb0bbaa4f9af:acd65c6b@us-cdbr-east-06.cleardb.net/heroku_ec7f6ec0f6cd80c?reconnect=true
+ 
 
   dbConn.connect(function (err) {
     if (err) {
